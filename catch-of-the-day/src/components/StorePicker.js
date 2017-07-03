ec43079c45ae64b70c597df1ad0316f2c991ce06
Dylan Sheffer <dsheffer16@Dylans-MacBook-Pro.local>
2017-07-03 14:14:52 -0400

    Created events, got DOM elements, and gave an example on the constructor syntax for components.
    #ep11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import{ getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+    // Could use the constructor syntax to add the goToStore method to the StorePicker component
+    // constructor(){
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        //first grab text
+        console.log(this.storeInput.value);
+        //second change route to /store/:storeid
+    }
+    
     render() {
         return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
             <h2>Please Enter a Store</h2>
-            <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
             <button type="submit">Visit Store</button>
         </form>
         )
